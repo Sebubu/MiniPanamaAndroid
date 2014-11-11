c555e3ebf32f810970abf94fa2a09aa19935d4ea
Murthy10 <samuel.kurath@gmail.com>
2014-11-11 17:46:01 +0100

    modified:   app/app.iml 	modified:   app/src/main/AndroidManifest.xml 	modified:   app/src/main/java/ch/hsr/sevi/minipanamaandroid/LoginActivity.java 	modified:   app/src/main/java/ch/hsr/sevi/minipanamaandroid/MainActivity.java 	new file:   app/src/main/java/ch/hsr/sevi/minipanamaandroid/RegistrationActivity.java 	modified:   app/src/main/res/layout/activity_main.xml 	new file:   app/src/main/res/layout/activity_registration.xml 	new file:   app/src/main/res/menu/menu_registration.xml 	modified:   app/src/main/res/values/strings.xml

--- app/src/main/java/ch/hsr/sevi/minipanamaandroid/LoginActivity.java
+++ app/src/main/java/ch/hsr/sevi/minipanamaandroid/LoginActivity.java
@@ -1,9 +1,12 @@
 package ch.hsr.sevi.minipanamaandroid;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends Activity {
@@ -12,6 +15,13 @@ public class LoginActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final Button button = (Button) findViewById(R.id.buttonRegistration);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),RegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
--- app/src/main/java/ch/hsr/sevi/minipanamaandroid/RegistrationActivity.java
+++ app/src/main/java/ch/hsr/sevi/minipanamaandroid/RegistrationActivity.java
@@ -1,36 +1,24 @@
 package ch.hsr.sevi.minipanamaandroid;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
-import ch.hsr.sevi.library.*;
 
-public class MainActivity extends Activity {
+public class RegistrationActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.activity_registration);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
         return true;
     }
 
