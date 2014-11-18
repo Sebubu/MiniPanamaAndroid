c6c600365caacb56cb7e59f56ce96c5ea3f2ffbe
Severin Bühler <sevi_buehler@hotmail.com>
2014-11-18 11:04:15 +0100

    ...

--- app/src/main/java/ch/hsr/sevi/view/RegistrationActivity.java
+++ app/src/main/java/ch/hsr/sevi/view/RegistrationActivity.java
@@ -4,14 +4,41 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class RegistrationActivity extends Activity {
+
+    Button btnRegister;
+    TextView txtMail;
+    TextView txtMatrikel;
+    TextView txtPw1;
+    TextView txtPw2;
+    TextView txtValidate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+        btnRegister = (Button) this.findViewById( R.id.buttonRegistration);
+        txtMail = (EditText) this.findViewById(R.id.textEmail);
+        txtMatrikel = (EditText) this.findViewById(R.id.textMartrikelNummer);
+        txtPw1 = (EditText) this.findViewById(R.id.textPasswordReg);
+        txtPw2 = (EditText) this.findViewById(R.id.textPasswordReg2);
+txtValidate = (EditText) this.findViewById(R.id.text);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
 
