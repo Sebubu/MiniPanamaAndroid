8afadbdbeb33ed1cba86360348bd3803c4033324
Murthy10 <samuel.kurath@gmail.com>
2014-11-19 12:52:42 +0100

    with Table

--- app/src/main/java/ch/hsr/sevi/view/MainActivity.java
+++ app/src/main/java/ch/hsr/sevi/view/MainActivity.java
@@ -7,29 +7,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ch.hsr.sevi.bl.Loan;
+import ch.hsr.sevi.bl.Reservation;
 import ch.hsr.sevi.library.*;
+import ch.hsr.sevi.view.R;
 
 public class MainActivity extends Activity {
 
-    //sku exapmple to use library
-
+    private TableLayout tblReservations;
+    private TableLayout tblLoans;
+    private List<Reservation> itemsReservations = new ArrayList<Reservation>();
+    private List<Loan> itemsLoans = new ArrayList<Loan>();
+    private Callback<Boolean> registerCallback;
+    private Callback<Boolean> loginCallback;
+    private Callback<List<Reservation>> reservationsCallback;
+    private Callback<List<Loan>> loansCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initCallbacks();
 
-        System.out.println("test");
-
-        Callback<Boolean> registerCallback = new Callback<Boolean>() {
-            @Override
-            public void notfiy(Boolean input) {
-
-                System.out.println("Registred" + input);
-            }
-        };
-        LibraryService.register("test@hsr.ch", "password", "name", "studentNumber", registerCallback);
+        //LibraryService.register("test@hsr.ch", "password", "name", "studentNumber", registerCallback);
+        LibraryService.login("m@hsr.ch", "12345", loginCallback);
 
     }
 
@@ -54,5 +63,95 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initCallbacks(){
+        registerCallback = new Callback<Boolean>() {
+            @Override
+            public void notfiy(Boolean input) {
+
+            }
+        };
+
+        loginCallback = new Callback<Boolean>() {
+            @Override
+            public void notfiy(Boolean input) {
+                if(LibraryService.IsLoggedIn()){
+                    LibraryService.getReservationsForCustomer(reservationsCallback);
+                    LibraryService.getLoansForCustomer(loansCallback);
+                }
+            }
+        };
+
+        loansCallback = new Callback<List<Loan>>() {
+            @Override
+            public void notfiy(List<Loan> input) {
+                if(!input.isEmpty()) {
+                    itemsLoans = input;
+                    fillLoans();
+                }
+            }
+        };
+
+        reservationsCallback = new Callback<List<Reservation>>() {
+            @Override
+            public void notfiy(List<Reservation> input) {
+                if(!input.isEmpty()){
+                    itemsReservations = input;
+                    fillReservations();
+                }
+            }
+        };
+    }
+
+    private void fillReservations(){
+        tblReservations = (TableLayout) findViewById(R.id.tblReservations);
+        for(Reservation r: itemsReservations)
+        {
+            String gadget = r.getGadget().getName();
+            String date = r.getReservationDate().toString();
+
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.FILL_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+
+            TextView tvGadget = new TextView(this);
+            tvGadget.setText(gadget);
+            tr.addView(tvGadget);
+
+            TextView tvDate = new TextView(this);
+            tvDate .setText(date);
+            tr.addView(tvDate );
+
+            tblReservations.addView(tr, new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.FILL_PARENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT));
+        }
+    }
+
+    private void fillLoans(){
+        tblLoans = (TableLayout) findViewById(R.id.tblLoans);
+        for(Loan l: itemsLoans)
+        {
+            String gadget = l.getGadget().getName();
+            String date = l.getPickupDate().toString();
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.FILL_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+
+            TextView tvGadget = new TextView(this);
+            tvGadget.setText(gadget);
+            tr.addView(tvGadget);
+
+            TextView tvDate = new TextView(this);
+            tvDate .setText(date);
+            tr.addView(tvDate );
+
+            tblLoans.addView(tr, new TableLayout.LayoutParams(
+                    TableLayout.LayoutParams.FILL_PARENT,
+                    TableLayout.LayoutParams.WRAP_CONTENT));
+        }
     }
 }
